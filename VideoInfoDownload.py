2979c66f8dbc44b182e08970f2fe48fd044253af
Angelique Husson <44775743+AngeliqueHusson@users.noreply.github.com>
2020-04-16 14:21:05 +1000

    VideoId changes

--- VideoInfoDownload.py
+++ VideoInfoDownload.py
@@ -5,16 +5,17 @@ Created on Sun Mar  8 13:53:48 2020
 @author: HUI
 """
 
-
 import os.path
 import requests
 import json
 import pickle
-from urllib.request import urlopen 
+from urllib.request import urlopen
 
+cwd = os.getcwd()
+print(cwd)
 
 ### Key.txt includes my YouTube API key
-file = open("Key.txt") 
+file = open("Data/Key.txt")
 key = (file.read()).rstrip("\n")
 
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
@@ -23,10 +24,10 @@ DEVELOPER_KEY = key
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-with open("VideoID.txt", "rb") as fp:   # Unpickling
+with open("Data/VideoID.txt", "rb") as fp:   # Unpickling
     ID_list = pickle.load(fp)
     
-json_save_path = 'E:/UQ/DATA7901/Project/video_info'  ##set your own path
+json_save_path = 'C:/Users/s157165/Documents/Jaar 5 2019-2020 Master/Internship Australia/InternshipOneOnEpsilon/Data/video_info'  ##set your own path
 
 ### download video info    Output is stored in Data/video_info
 for ID in ID_list:
@@ -47,12 +48,12 @@ videoMetadata = [] #declaring our list
 for video in videos['items']: 
     if video['kind'] == 'youtube#video':
         print("Upload date:        "+video['snippet']['publishedAt'])    # Here the upload date of the specific video is listed
-        print("Number of views:    "+video['statistics']['viewCount']) # Here the number of views of the specific video is listed
+        print("Number of views:    "+video['statistics']['viewCount'])  # Here the number of views of the specific video is listed
         print( "Number of likes:    "+video['statistics']['likeCount'])  # etc
-        print ("Number of dislikes: "+video['statistics']['dislikeCount'])
-        print ("Number of favorites:"+video['statistics']['favoriteCount'])
-        print ("Number of comments: "+video['statistics']['commentCount'])
-        print ("\n")
+        print("Number of dislikes: "+video['statistics']['dislikeCount'])
+        print("Number of favorites:"+video['statistics']['favoriteCount'])
+        print("Number of comments: "+video['statistics']['commentCount'])
+        print("\n")
 
 
 
