9ec9f3752c4c15c4652eb45745418eab94dca07f
Angelique Husson <44775743+AngeliqueHusson@users.noreply.github.com>
2020-05-01 14:40:22 +1000

    adding neural networks

--- Neural Network Recurrent.py
+++ Neural Network Recurrent.py
@@ -12,15 +12,15 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
 from keras.callbacks import EarlyStopping
-import cufflinks
-from IPython.core.interactiveshell import InteractiveShell
 import matplotlib.pyplot as plt
 from Feature_extraction import x_train_tfidf1, x_train_tfidf, vectorizer
+from keras.preprocessing.text import Tokenizer
+from sklearn.model_selection import train_test_split
 
 # Directory and data import
 # Change to your own directory
-directory = 'C:/Users/Nikki/Desktop/Internship AM/Input data classification/YouTube-video-info-download-including-title-channel-automatically-generated-subtitles--master/Data'
-#directory = 'C:/Users/s157165/Documents/Jaar 5 2019-2020 Master/Internship Australia/InternshipOneOnEpsilon/Data'
+# directory = 'C:/Users/Nikki/Desktop/Internship AM/Input data classification/YouTube-video-info-download-including-title-channel-automatically-generated-subtitles--master/Data'
+directory = 'C:/Users/s157165/Documents/Jaar 5 2019-2020 Master/Internship Australia/InternshipOneOnEpsilon/Data'
 os.chdir(directory)
 
 # Obtaining training and validation data
@@ -29,31 +29,41 @@ validation = pd.read_csv("validation.csv")
 trainingBig = pd.read_csv("trainingbig.csv")
 category_id_df = pd.read_csv("category_id_df.csv")
 
-X = pad_sequences(x_train_tfidf1, maxlen=None)
-
 EMBEDDING_DIM = 100
 MAX_NB_WORDS = x_train_tfidf1.shape[1]
 
-X_train = x_train_tfidf
-X_test = vectorizer.transform(validation['x_val'])
-Y_train = training["y_train"]
-Y_test = validation["y_val"]
+tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+tokenizer.fit_on_texts(trainingBig["x_trainBig"])
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+X = tokenizer.texts_to_sequences(trainingBig["x_trainBig"])
+X = pad_sequences(X, maxlen=250)
+Y = pd.get_dummies(trainingBig["y_trainBig"])
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, random_state=12)
+
+print(X_train.shape, Y_train.shape)
+print(X_test.shape, Y_test.shape)
+
+print(Y_train)
 
 #Neural Network model
 model = Sequential()
 model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
 model.add(SpatialDropout1D(0.2))
 model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(13, activation='softmax'))
+# model.add(Dense(100, activation='relu', input_shape=X.shape))
+# model.add(Dense(100, activation='relu'))
+model.add(Dense(11, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-epochs = 5
+epochs = 50
 batch_size = 64
 
 history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size,validation_split=0.1,callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
 
-accr = model.evaluate(X_test,Y_test)
-print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
+acc = model.evaluate(X_test, Y_test)
+print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(acc[0], acc[1]))
 
 plt.title('Loss')
 plt.plot(history.history['loss'], label='train')
@@ -62,7 +72,7 @@ plt.legend()
 plt.show();
 
 plt.title('Accuracy')
-plt.plot(history.history['acc'], label='train')
-plt.plot(history.history['val_acc'], label='test')
+plt.plot(history.history['accuracy'], label='train')
+plt.plot(history.history['val_accuracy'], label='test')
 plt.legend()
 plt.show();
