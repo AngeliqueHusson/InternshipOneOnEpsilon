07653fffe4f599b1c6fabaf1a4fb7af8c0072757
NikkiLeijnse <63827542+NikkiLeijnse@users.noreply.github.com>
2020-05-01 13:27:48 +1000

    Add files via upload

---
+++ Neural Network Recurrent.py
@@ -0,0 +1,68 @@
+"""
+    Naive Bayes Method using the tf-idf feature extraction method
+    This file uses as input the file created in the 'JoinTextTohashtag.py' file.
+
+    @authors Angelique Husson & Nikki Leijnse
+"""
+
+# Neural Network method
+import os
+import pandas as pd
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from keras.callbacks import EarlyStopping
+import cufflinks
+from IPython.core.interactiveshell import InteractiveShell
+import matplotlib.pyplot as plt
+from Feature_extraction import x_train_tfidf1, x_train_tfidf, vectorizer
+
+# Directory and data import
+# Change to your own directory
+directory = 'C:/Users/Nikki/Desktop/Internship AM/Input data classification/YouTube-video-info-download-including-title-channel-automatically-generated-subtitles--master/Data'
+#directory = 'C:/Users/s157165/Documents/Jaar 5 2019-2020 Master/Internship Australia/InternshipOneOnEpsilon/Data'
+os.chdir(directory)
+
+# Obtaining training and validation data
+training = pd.read_csv("training.csv")
+validation = pd.read_csv("validation.csv")
+trainingBig = pd.read_csv("trainingbig.csv")
+category_id_df = pd.read_csv("category_id_df.csv")
+
+X = pad_sequences(x_train_tfidf1, maxlen=None)
+
+EMBEDDING_DIM = 100
+MAX_NB_WORDS = x_train_tfidf1.shape[1]
+
+X_train = x_train_tfidf
+X_test = vectorizer.transform(validation['x_val'])
+Y_train = training["y_train"]
+Y_test = validation["y_val"]
+
+#Neural Network model
+model = Sequential()
+model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
+model.add(SpatialDropout1D(0.2))
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(13, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+epochs = 5
+batch_size = 64
+
+history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size,validation_split=0.1,callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
+
+accr = model.evaluate(X_test,Y_test)
+print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
+
+plt.title('Loss')
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.legend()
+plt.show();
+
+plt.title('Accuracy')
+plt.plot(history.history['acc'], label='train')
+plt.plot(history.history['val_acc'], label='test')
+plt.legend()
+plt.show();
